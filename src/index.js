181fd0817bc2e2c10bb3f2682c32b1da5eee015b
ntwigamartin <ntwiga.martin@gmail.com>
2022-12-01 22:01:31 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  handleTasks(e.target.newt.value)
+  form.reset()
+  }) // your code here
 });
+
+function handleTasks(todo) {
+  let list = document.querySelector("#tasks")
+  let li = document.createElement('li')
+  list.appendChild(li).textContent = `${todo} `
+  console.log(li)
+  let btn = document.createElement('button')
+  li.appendChild(btn).textContent = "X "
+  btn.addEventListener('click', (e)=>{
+    e.target.parentNode.remove()
+  })
+  let selct = document.createElement('select')
+  li.appendChild(selct)
+  let optV = document.createElement('option')
+  selct.appendChild(optV).textContent = "High"
+  
+}
